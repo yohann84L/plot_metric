6545a1994243941e605eaab08efc55861cd7318f
Yohann Lereclus <lereclus84L@gmail.com>
2019-08-27 15:11:02 +0200

    added dependency into setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='plot_metric',
-    version='0.0.4',
+    version='0.0.5',
     scripts=['plot_metric_package'],
     author="Yohann Lereclus",
     author_email="lereclus84L@gmail.com",
@@ -15,6 +15,15 @@ setuptools.setup(
     url="https://github.com/yohann84L/plot_metric/",
     packages=setuptools.find_packages(),
     py_modules=['plot_metric/functions'],
+    install_requires=[
+        "scipy>=1.1.0",
+        "matplotlib>=3.0.2",
+        "colorlover>=0.3.0",
+        "pandas>=0.23.4",
+        "seaborn>=0.9.0",
+        "numpy>=1.15.4",
+        "scikit_learn>=0.21.2",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
